32d117832c1ed8b8a429fde00558a62a5e095fc0
Vitor221 <vitornascimento6650@gmail.com>
2023-08-08 15:20:06 -0300

    Java exemplos Date

--- matrizJava/src/application/Program.java
+++ matrizJava/src/application/Program.java
@@ -1,37 +1,47 @@
 package application;
 
-import java.util.Scanner;
+import java.time.*;
+import java.time.temporal.ChronoUnit;
 
 public class Program {
     public static void main(String[] args)
     {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+        LocalDate d04 = LocalDate.parse("2022-07-20");
+        LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
+        Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
 
-        int[][] mat = new int[N][M];
+        LocalDate pastWeekLocalDate = d04.minusDays(7);
+        LocalDate nextWeekLocalDate = d04.plusDays(7);
 
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < M; j++) {
-                mat[i][j] = sc.nextInt();
-            }
-        }
+        System.out.println("pastWeekLocalDate = " + pastWeekLocalDate);
+        System.out.println("nextWeekLocalDate = " + nextWeekLocalDate);
 
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < M; j++) {
-                System.out.print(" " + mat[i][j]);
-            }
-            System.out.println();
-        }
+        System.out.println();
 
-        int X = sc.nextInt();
+        LocalDateTime pastWeekLocalDateTime = d05.minusDays(7);
+        LocalDateTime nextWeekLocalDateTime = d05.plusDays(7);
 
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < M; j++) {
-                if(mat[i][j] == X) {
-                    System.out.println("Position " + i + "," + j + ":");
-                }
-            }
-        }
+        System.out.println("pastWeekLocalDateTime" + pastWeekLocalDateTime);
+        System.out.println("nextWeekLocalDateTime" + nextWeekLocalDateTime);
+
+        System.out.println();
+
+        Instant pastWeekInstant = d06.minus(7, ChronoUnit.DAYS);
+        Instant nextWeekInstant = d06.plus(7, ChronoUnit.DAYS);
+
+        System.out.println("pastWeekInstant" + pastWeekInstant);
+        System.out.println("nextWeekInstant" + nextWeekInstant);
+
+        System.out.println();
+
+        Duration t1 = Duration.between(pastWeekLocalDate.atStartOfDay(), d04.atStartOfDay());
+        Duration t2 = Duration.between(pastWeekLocalDateTime, d05);
+        Duration t3 = Duration.between(pastWeekInstant, d06);
+        Duration t4 = Duration.between(d06, pastWeekInstant);
+
+        System.out.println("t1 dias = " + t1.toDays());
+        System.out.println("t2 dias = " + t2.toDays());
+        System.out.println("t3 dias = " + t3.toDays());
+        System.out.println("t4 dias = " + t4.toDays());
     }
 }
