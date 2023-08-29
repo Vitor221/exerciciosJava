710f62a67a92db302ce70da2c74093a6e5f31f65
Vitor221 <vitornascimento6650@gmail.com>
2023-08-29 14:22:12 -0300

    Files

--- course/src/application/Program.java
+++ course/src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -8,22 +10,30 @@ public class Program {
 
     public static void main(String[] args) {
 
-        File file = new File("/home/vitor/AnotacoesJava");
-        Scanner sc = null;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a folder path: ");
+        String strPath = sc.nextLine();
 
-        try {
-            sc = new Scanner(file);
-            while(sc.hasNextLine()) {
-                System.out.println(sc.nextLine());
-            }
-        } catch(IOException e) {
-            System.out.println("Error: " + e.getMessage());
-        } finally {
-            if(sc != null) {
-                sc.close();
-            }
+        File path = new File(strPath);
+        File[] folders = path.listFiles(File::isDirectory);
+        System.out.println("FOLDERS: ");
+        for(File folder : folders) {
+            System.out.println(folder);
         }
 
+        System.out.println();
+
+        File[] files = path.listFiles(File::isFile);
+        System.out.println("FILES: ");
+        for (File file : files) {
+            System.out.println(file);
+        }
+
+        boolean success = new File(strPath + "/subdir").mkdir();
+        System.out.println();
+        System.out.println("Directory created successfully: " + success);
+
+        sc.close();
     }
 
 }
